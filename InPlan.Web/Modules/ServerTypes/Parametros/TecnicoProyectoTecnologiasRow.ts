34ed58a5154d70ca00e2ca959310c858dafc6774
Julian <julianmartinez_93@hotmail.com>
2023-07-13 13:39:11 -0300

    order entity

--- InPlan.Web/Modules/ServerTypes/Parametros/TecnicoProyectoTecnologiasRow.ts
+++ InPlan.Web/Modules/ServerTypes/Parametros/TecnicoProyectoTecnologiasRow.ts
@@ -1,4 +1,4 @@
-﻿import { fieldsProxy } from "@serenity-is/corelib/q";
+﻿import { getLookup, getLookupAsync, fieldsProxy } from "@serenity-is/corelib/q";
 
 export interface TecnicoProyectoTecnologiasRow {
     IdTecnicoProyectoTecnologia?: number;
@@ -11,6 +11,12 @@ export interface TecnicoProyectoTecnologiasRow {
 export abstract class TecnicoProyectoTecnologiasRow {
     static readonly idProperty = 'IdTecnicoProyectoTecnologia';
     static readonly localTextPrefix = 'Parametros.TecnicoProyectoTecnologias';
+    static readonly lookupKey = 'Parametros.TecnicoProyectoTecnologias';
+
+    /** @deprecated use getLookupAsync instead */
+    static getLookup() { return getLookup<TecnicoProyectoTecnologiasRow>('Parametros.TecnicoProyectoTecnologias') }
+    static async getLookupAsync() { return getLookupAsync<TecnicoProyectoTecnologiasRow>('Parametros.TecnicoProyectoTecnologias') }
+
     static readonly deletePermission = 'Administration:General';
     static readonly insertPermission = 'Administration:General';
     static readonly readPermission = 'Administration:General';
