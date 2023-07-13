41e950691c5420cf247b0c6437ac0e3e9aeb8815
Julian <85456149+Julian1993ARG@users.noreply.github.com>
2023-07-13 12:52:08 -0300

    order entity (#9)

--- InPlan.Web/Modules/Parametros/Clientes/ClientesForm.cs
+++ InPlan.Web/Modules/Parametros/Clientes/ClientesForm.cs
@@ -1,4 +1,4 @@
-﻿using Serenity.ComponentModel;
+using Serenity.ComponentModel;
 
 namespace InPlan.Parametros.Forms;
 
@@ -6,10 +6,16 @@ namespace InPlan.Parametros.Forms;
 [BasedOnRow(typeof(ClientesRow), CheckNames = true)]
 public class ClientesForm
 {
+    [HalfWidth]
     public int IdEmpresa { get; set; }
+    [HalfWidth]
     public int IdArea { get; set; }
+    [HalfWidth]
     public string DescripcionCorta { get; set; }
+    [HalfWidth]
     public string Descripcion { get; set; }
+    [TextAreaEditor(Rows = 5)]
     public string Observaciones { get; set; }
+    [HideOnInsert]
     public bool Baja { get; set; }
 }
