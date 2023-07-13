24f3b979d888b23bb859f0536fb6a227a2359ee4
Julian <julianmartinez_93@hotmail.com>
2023-07-13 13:39:57 -0300

    olvide guardar

--- InPlan.Web/Modules/Parametros/TecnicoProyectoTecnologias/TecnicoProyectoTecnologiasColumns.cs
+++ InPlan.Web/Modules/Parametros/TecnicoProyectoTecnologias/TecnicoProyectoTecnologiasColumns.cs
@@ -1,4 +1,4 @@
-﻿using Serenity.ComponentModel;
+using Serenity.ComponentModel;
 using System.ComponentModel;
 
 namespace InPlan.Parametros.Columns;
@@ -7,8 +7,8 @@ namespace InPlan.Parametros.Columns;
 [BasedOnRow(typeof(TecnicoProyectoTecnologiasRow), CheckNames = true)]
 public class TecnicoProyectoTecnologiasColumns
 {
-    [EditLink, DisplayName("Db.Shared.RecordId"), AlignRight]
-    public int IdTecnicoProyectoTecnologia { get; set; }
+    //[EditLink, DisplayName("Db.Shared.RecordId"), AlignRight]
+    //public int IdTecnicoProyectoTecnologia { get; set; }
     public string IdTecnicoProyectoObservaciones { get; set; }
     public string IdTecnologiaDescripcion { get; set; }
 }
