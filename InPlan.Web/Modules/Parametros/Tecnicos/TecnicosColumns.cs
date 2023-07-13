961ae7be45bdfab9d0d7e2e6accf74266fb263d4
Julian <85456149+Julian1993ARG@users.noreply.github.com>
2023-07-13 13:01:50 -0300

    order entity (#10)

--- InPlan.Web/Modules/Parametros/Tecnicos/TecnicosColumns.cs
+++ InPlan.Web/Modules/Parametros/Tecnicos/TecnicosColumns.cs
@@ -1,4 +1,4 @@
-﻿using Serenity.ComponentModel;
+using Serenity.ComponentModel;
 using System.ComponentModel;
 
 namespace InPlan.Parametros.Columns;
@@ -7,14 +7,17 @@ namespace InPlan.Parametros.Columns;
 [BasedOnRow(typeof(TecnicosRow), CheckNames = true)]
 public class TecnicosColumns
 {
-    [EditLink, DisplayName("Db.Shared.RecordId"), AlignRight]
-    public int IdTecnico { get; set; }
+    //[EditLink, DisplayName("Db.Shared.RecordId"), AlignRight]
+    //public int IdTecnico { get; set; }
+    [Width(100)]
     public string IdEmpresaDescripcionCorta { get; set; }
+    [Width(150)]
     public string IdAreaDescripcion { get; set; }
-    [EditLink]
+    [EditLink, Width(150)]
     public string NombreCompleto { get; set; }
     public string Celular { get; set; }
     public string Email { get; set; }
-    public string Observaciones { get; set; }
+    //public string Observaciones { get; set; }
+    [Width(80)]
     public bool Baja { get; set; }
 }
