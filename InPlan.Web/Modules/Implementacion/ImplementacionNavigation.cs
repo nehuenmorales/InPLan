07dd210e7eed0ced2eb1e64b5f64de143051df4b
nehuenmorales <nehuenmorales640@gmail.com>
2023-07-18 20:48:12 -0300

    sidebar

--- InPlan.Web/Modules/Implementacion/ImplementacionNavigation.cs
+++ InPlan.Web/Modules/Implementacion/ImplementacionNavigation.cs
@@ -1,7 +1,7 @@
 using Serenity.Navigation;
 using MyPages = InPlan.Implementacion.Pages;
 
-[assembly: NavigationMenu(1000, "Implementación", icon: "fa-sitemap")]
+[assembly: NavigationMenu(1000, "Proyectos", icon: "fas fa-project-diagram")]
 
-[assembly: NavigationLink(1100, "Implementación/Implementaciones", typeof(MyPages.ImplementacionesPage), icon: "fa-wrench")]
-[assembly: NavigationLink(1200, "Implementación/Tipos Implementacion", typeof(MyPages.TiposImplementacionPage), icon: "fa-puzzle-piece")]
+[assembly: NavigationLink(1100, "Proyectos/Implementaciones", typeof(MyPages.ImplementacionesPage), icon: "fa-wrench")]
+[assembly: NavigationLink(1200, "Proyectos/Tipos Implementacion", typeof(MyPages.TiposImplementacionPage), icon: "fa-puzzle-piece")]
